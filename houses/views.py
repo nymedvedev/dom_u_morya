8788aa2c4bd3b4723443410b84a7957137758d9e
nymedvedev <137264272+nymedvedev@users.noreply.github.com>
2023-09-15 13:22:13 +0300

    1. Для вывода домов на стр. бр-ра, т.к. дома хранятся в БД, а доступ к БД ч/з модели: импортировал модель: from houses.models import House 2. Создал запрос к БД (исп.ORM) на получение всех объектов (домов): houses = House.objects.all() 3. Добавил в функцию render словарь для вывода данных в html:  return render(request, "houses/houses_list.html", {"houses": houses})

--- houses/views.py
+++ houses/views.py
@@ -1,9 +1,21 @@
 # ФАЙЛ С ЛОГИКОЙ РАБОТЫ ПРИЛОЖЕНИЯ
 
 from django.shortcuts import render
+# импортируем модель, т.к. дома (которые будем отображать на сайте,
+# хранятся в БД, а доступ к БД через модели)
+from houses.models import House
+
+
 # Ф-ЦИЯ (называется "представление") ГЕНЕРИРУЕТ HTML-стр. И ВОЗВРАЩАЕТ ЕЁ ЮЗЕРУ:
 def houses_list(request):
+    houses = House.objects.all()
+    # выведем инфо о домах в консоль:
+    for house in houses:
+        print(house.name, house.price)
     # свяжем представление (houses_list) c шаблоном (houses_list.html)
-    return render(request, "houses/houses_list.html")
+    return render(request, "houses/houses_list.html", {"houses": houses})
 # Ф-ЦИЯ houses_list ВОЗВРАЩАЕТ РЕЗУЛЬТАТ ФУНКЦИИ render,
-# КОТОРАЯ ПРИНИМАЕТ: 1) ЗАПРОС (request) ЮЗЕРА, 2) НАЗВ. ШАБЛОНА ("houses/houses_list.html")
+# КОТОРАЯ ПРИНИМАЕТ:
+# 1) ЗАПРОС (request) ЮЗЕРА,
+# 2) НАЗВ. ШАБЛОНА ("houses/houses_list.html")
+# 3) словарь для вывода данных в html (на сайт)
