01ab70feee839a3b3ecb949f734195013affe536
nymedvedev <137264272+nymedvedev@users.noreply.github.com>
2023-09-19 11:06:33 +0300

    1. Добавил в views.py модуль get_object_or_404 2. Добавил представление для отобр. стр. с отд. домом. 3. Подключил к представлению шаблон house_detail.html 4. Создал шаблон house_detail.html для стр.с отд. домом.

--- houses/views.py
+++ houses/views.py
@@ -1,8 +1,10 @@
 # ФАЙЛ С ЛОГИКОЙ РАБОТЫ ПРИЛОЖЕНИЯ
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+#импортируем get_object_or_404, чтобы
+
 # импортируем модель, т.к. дома (которые будем отображать на сайте,
-# хранятся в БД, а доступ к БД через модели)
+# хранятся в БД, а доступ к БД через модели).
 from houses.models import House
 
 
@@ -14,8 +16,18 @@ def houses_list(request):
         print(house.name, house.price)
     # свяжем представление (houses_list) c шаблоном (houses_list.html)
     return render(request, "houses/houses_list.html", {"houses": houses})
-# Ф-ЦИЯ houses_list ВОЗВРАЩАЕТ РЕЗУЛЬТАТ ФУНКЦИИ render,
-# КОТОРАЯ ПРИНИМАЕТ:
-# 1) ЗАПРОС (request) ЮЗЕРА,
-# 2) НАЗВ. ШАБЛОНА ("houses/houses_list.html")
-# 3) словарь для вывода данных в html (на сайт)
+    # Ф-ЦИЯ houses_list ВОЗВРАЩАЕТ РЕЗУЛЬТАТ ФУНКЦИИ render,
+    # КОТОРАЯ ПРИНИМАЕТ:
+    # 1) ЗАПРОС (request) ЮЗЕРА,
+    # 2) НАЗВ. ШАБЛОНА ("houses/houses_list.html")
+    # 3) словарь для вывода данных в html (на сайт)
+
+# добавим представление для отобр. отд. стр. с домами:
+def house_list(request, house_id):
+    # присваиваем переменной house рез-тат раб.функ. get_object_or_404,
+    # кот-ая приним. модель House и пар-тр id, по кот-му б/т поиск
+    # нужн. объекта. Если найден - функ.верн. его в перем. House, нет - 404:
+    house = get_object_or_404(House, id=house_id)
+    # подключу к представлению шаблон:
+    return render(request, "houses/house_detail.html", {"house": house})
+
