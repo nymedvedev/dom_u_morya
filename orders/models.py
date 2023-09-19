4bad61453aa941ed533738f2df42c0c9520f2682
nymedvedev <137264272+nymedvedev@users.noreply.github.com>
2023-09-19 14:49:52 +0300

    1. Создал приложение orders для принятия на сайте заявок. 2. Создал модель Order с полями (имя клиента, телефон, дата заявки) заявки.

---
+++ orders/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+# импортирую модель приложения House:
+from houses.models import House
+
+# создаю внешний ключ на дом = модель Order связана с House,
+# заявка б/т хранить идент-р дома, к к-му заявка отн-ся.
+class Order(models.Model):
+    house = models.ForeignKey(House, verbose_name="дом", on_delete=models.CASCADE)
+    # on_delete - при удалении дома удаляются заявки этого дома
+    #добавлю поле name для имени потенц.клиента:
+    name = models.CharField("имя", max_length=50)
+    phone = models.CharField("телефон", max_length=50)
+    # дата заявки автоматически б/т заполнено актуальн. датой:
+    date = models.DateTimeField("дата", auto_now_add=True)
+
+
