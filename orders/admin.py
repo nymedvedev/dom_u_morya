1155b254a03bb191bc2d02b9c28217cdd78e4dd6
nymedvedev <137264272+nymedvedev@users.noreply.github.com>
2023-09-20 10:47:35 +0300

    Сделал админку для обработки заказов из форм, к-ые заполн.клиенты.

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,11 @@
+# ФАЙЛ ДЛЯ СОЗДАНИЯ АДМИНКИ ДЛЯ ОБРАБОТКИ ЗАКАЗОВ
 from django.contrib import admin
 
-# Register your models here.
+# 1) импортирую модель
+from orders.models import Order
+
+# 3) дабавил декоратор:
+@admin.register(Order)
+# 2) создаю класс:
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["name", "phone", "house", "date"]
