291fd4578ea50999a314908624928959927eac51
nymedvedev <137264272+nymedvedev@users.noreply.github.com>
2023-09-21 17:41:54 +0300

    Отредактировал текст соглашения.

--- orders/forms.py
+++ orders/forms.py
@@ -11,7 +11,7 @@ from houses.models import House
 # т.к. я хочу создать форму на осн. модели:
 class OrderForm(forms.ModelForm):
     # добавим согласие юзера с обр.перс.д.:
-    personal_data = forms.BooleanField(label="Я согласен (/согласна) на обработку персональных данных.", required=True)
+    personal_data = forms.BooleanField(label="Я согласен/-cна обработку персональных данных.", required=True)
     # скроем возм.выбора дома:
     house = forms.ModelChoiceField(queryset=House.objects.all(), widget=forms.HiddenInput)
     # queryset в кач-ве фильтра, widget - настройка отображения
